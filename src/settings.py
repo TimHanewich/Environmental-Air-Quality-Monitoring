b409b34a4b05915b9a2526112f209db185d83eaa
Tim Hanewich <tahanewich@live.com>
2024-06-09 19:37:09 -0400

    Updated settings

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,17 @@
+# I2C settings (for the ENS160)
+i2c_bus:int = 1
+i2c_sda:int = 16
+i2c_scl:int = 17
+
+# DHT-22 settings
+dht22_gpio:int = 10
+
+# Wifi login
 ssid = ""
 password = ""
+
+# sample time (how many seconds should elapse in between sampling and uploading)
 sample_time_seconds:int = 60
+
+# What URL to make an HTTP POST request to with the sampled data
 post_url:str = ""

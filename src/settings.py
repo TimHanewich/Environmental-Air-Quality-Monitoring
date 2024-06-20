cceb01f0df2443f63a2006430e277ca4fb288a91
Tim Hanewich <tahanewich@live.com>
2024-06-20 11:18:10 -0400

    Added post fail tolerance to settings

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 # I2C settings (for the ENS160)
-i2c_bus:int = 1
+i2c_bus:int = 0
 i2c_sda:int = 16
 i2c_scl:int = 17
 
@@ -15,3 +15,8 @@ sample_time_seconds:int = 60
 
 # What URL to make an HTTP POST request to with the sampled data
 post_url:str = ""
+
+# post fail tolerance
+# this is the # of times a POST request can be made and not accepted (not a 2XX) response by the server. 
+# once the script fails this many times consecutively, it stops entirely and goes into an infinitely-repeating error mode
+post_fail_tolerance:int = 10
